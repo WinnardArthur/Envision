64eb9e30e272cc668416cf8ef26ee82e7f5cc9a4
Winnard Arthur <winnardarthur7@gmail.com>
2023-08-08 09:16:12 +0000

    feat: add charts and chart title component

--- client/src/components/Row1.tsx
+++ client/src/components/Row1.tsx
@@ -1,16 +1,110 @@
-import DashboardBox from './DashboardBox';
-import { useGetKpisQuery } from '@/state/api';
+import DashboardBox from "./DashboardBox";
+import { useGetKpisQuery } from "@/state/api";
+import { useMemo } from "react";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+import { useTheme } from "@mui/material";
 
-const Row1 = () => { 
-    const { data } = useGetKpisQuery();
+const Row1 = () => {
+  const { palette } = useTheme();
+  const { data } = useGetKpisQuery();
+  console.log("data", data);
+  const revenueExpenses = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+          expenses: expenses,
+        };
+      })
+    );
+  }, [data]);
 
   return (
     <>
-      <DashboardBox gridArea="a">Dashboard</DashboardBox>
+      <DashboardBox gridArea="a">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={revenueExpenses}
+            margin={{
+              top: 15,
+              right: 25,
+              left: -10,
+              bottom: 60,
+            }}
+          >
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.5}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+              <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.5}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+            </defs>
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              domain={[8000, 23000]}
+              tickLine={false}
+              axisLine={{ strokeWidth: "0" }}
+              style={{ fontSize: "10px" }}
+            />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="revenue"
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorRevenue)"
+              dot={true}
+            />
+            <Area
+              type="monotone"
+              dataKey="expenses"
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorExpenses)"
+              dot={true}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </DashboardBox>
       <DashboardBox gridArea="b">Dashboard</DashboardBox>
       <DashboardBox gridArea="c">Dashboard</DashboardBox>
     </>
   );
-}
+};
 
-export default Row1
+export default Row1;
